1e9c64404efc4bd471ebb187856921b01a695ad5
raviddog <raviddog@outlook.com>
2022-10-03 12:05:33 +0000

    :parra:

--- parra.py
+++ parra.py
@@ -8,7 +8,7 @@ def hello_world():
     page = urllib.request.urlopen('https://location.am-all.net/alm/location?gm=98&lang=en&ct=1012').read().decode("utf8").lower()
     if page.find('parramatta') > -1:
         return 'Yes'
-    return 'No'
+    return '<img src="https://media.discordapp.net/stickers/1013703451823308832.png"></img>'
 
 if __name__ == '__main__':
     app.run()
